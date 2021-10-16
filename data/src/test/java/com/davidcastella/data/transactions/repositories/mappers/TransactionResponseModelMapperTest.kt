c2831363314530fa26ba37c6e7dc8aee5f636693
davidkaste <hola@davidcastella.com>
2021-10-16 19:22:13 +0200

    Implemented transaction mapper test

--- data/src/test/java/com/davidcastella/data/transactions/repositories/mappers/TransactionResponseModelMapperTest.kt
+++ data/src/test/java/com/davidcastella/data/transactions/repositories/mappers/TransactionResponseModelMapperTest.kt
@@ -1,19 +1,21 @@
 package com.davidcastella.data.transactions.repositories.mappers
 
-import org.junit.Before
+import com.davidcastella.gnb_api.models.TransactionResponseModel
+import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class TransactionResponseModelMapperTest {
 
-    private lateinit var mapper: TransactionResponseModelMapper
+    private val mapper = TransactionResponseModelMapper()
 
-    @Before
-    fun setUp() {
-        mapper = TransactionResponseModelMapper()
-    }
+    private val transactionResponseModel = TransactionResponseModel("skuX", 32.3, "EUR")
 
     @Test
     operator fun invoke() {
+        val result = mapper(transactionResponseModel)
 
+        assertEquals(transactionResponseModel.sku, result.sku)
+        assertEquals(transactionResponseModel.amount.toBigDecimal(), result.amount)
+        assertEquals(transactionResponseModel.currency, result.currency)
     }
 }

188c8461dcab30b042f2ce23dc50f930f1e19654
davidkaste <hola@davidcastella.com>
2021-11-04 23:58:36 +0100

    Deal with functional structures in GetTransactions

--- domain/src/main/java/com/davidcastella/domain/transactions/interactors/GetTransactions.kt
+++ domain/src/main/java/com/davidcastella/domain/transactions/interactors/GetTransactions.kt
@@ -1,9 +1,12 @@
 package com.davidcastella.domain.transactions.interactors
 
+import arrow.core.Either
+import arrow.core.flatMap
 import com.davidcastella.domain.conversionrates.entities.CurrencyCode
 import com.davidcastella.domain.conversionrates.repositories.ConversionRatesRepository
 import com.davidcastella.domain.conversionrates.util.getConversionRate
 import com.davidcastella.domain.core.FlowUseCase
+import com.davidcastella.domain.core.failure.Failure
 import com.davidcastella.domain.transactions.entities.Transaction
 import com.davidcastella.domain.transactions.repositories.TransactionsRepository
 import kotlinx.coroutines.flow.Flow
@@ -13,19 +16,23 @@ import java.math.RoundingMode
 class GetTransactions(
     private val repository: TransactionsRepository,
     private val conversionRatesRepository: ConversionRatesRepository
-) : FlowUseCase<CurrencyCode, List<Transaction>> {
-    override fun invoke(currencyCode: CurrencyCode): Flow<List<Transaction>> =
+) : FlowUseCase<CurrencyCode, Either<Failure, List<Transaction>>> {
+    override fun invoke(currencyCode: CurrencyCode): Flow<Either<Failure, List<Transaction>>> =
         repository.getTransactions()
-            .combine(conversionRatesRepository.getConversionRates()) { transactionList, conversionRates ->
-                transactionList.map {
-                    if (it.currency == currencyCode.toString()) return@map it
-                    val rate =
-                        conversionRates.getConversionRate(it.currency, currencyCode.toString())
-                    Transaction(
-                        it.sku,
-                        (rate * it.amount).setScale(2, RoundingMode.HALF_EVEN),
-                        currencyCode.toString()
-                    )
+            .combine(conversionRatesRepository.getConversionRates()) { transactionEither, conversionEither ->
+                // flatMap :: Either<F, A> -> ((v: A) -> Either<F, B) -> Either<F, B>
+                // map :: Either<F, A> -> ((v: A) -> B) -> Either<F, B>
+                transactionEither.flatMap { transactionList ->
+                    conversionEither.map { conversionList ->
+                        transactionList.map {
+                            val rate = conversionList.getConversionRate(it.currency, currencyCode.toString())
+                            Transaction(
+                                it.sku,
+                                (rate * it.amount).setScale(2, RoundingMode.HALF_EVEN),
+                                currencyCode.toString()
+                            )
+                        }
+                    }
                 }
             }
 }

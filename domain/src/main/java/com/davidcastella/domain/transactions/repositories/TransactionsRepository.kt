eb0eb65debee322efc2a07f6e529cfbeaccfc943
davidkaste <hola@davidcastella.com>
2021-11-04 23:26:03 +0100

    Updated method signature in repositories definitions

--- domain/src/main/java/com/davidcastella/domain/transactions/repositories/TransactionsRepository.kt
+++ domain/src/main/java/com/davidcastella/domain/transactions/repositories/TransactionsRepository.kt
@@ -1,8 +1,10 @@
 package com.davidcastella.domain.transactions.repositories
 
+import arrow.core.Either
+import com.davidcastella.domain.core.failure.Failure
 import com.davidcastella.domain.transactions.entities.Transaction
 import kotlinx.coroutines.flow.Flow
 
 interface TransactionsRepository {
-    fun getTransactions(): Flow<List<Transaction>>
+    fun getTransactions(): Flow<Either<Failure, List<Transaction>>>
 }

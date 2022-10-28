e18322ebcf3b5c7191da7059da78241fcfb9c244
David Castellà <davidcpon@ext.zara.com>
2022-10-28 14:28:49 +0200

    Change scope to ViewModelComponent

--- data/src/main/java/com/davidcastella/data/conversionrates/di/module.kt
+++ data/src/main/java/com/davidcastella/data/conversionrates/di/module.kt
@@ -7,10 +7,10 @@ import com.davidcastella.domain.conversionrates.repositories.ConversionRatesRepo
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
-import dagger.hilt.components.SingletonComponent
+import dagger.hilt.android.components.ViewModelComponent
 
 @Module
-@InstallIn(SingletonComponent::class)
+@InstallIn(ViewModelComponent::class)
 abstract class ConversionRatesDataModule {
 
     @Binds

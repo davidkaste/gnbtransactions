cf24c1ebcd90652cdaee4fd50117c35048ab4951
David Castellà <hola@davidcastella.com>
2023-10-25 18:36:37 +0200

    ktlint'd file

--- data/src/test/java/com/davidcastella/data/conversionrates/repositories/ConversionRatesRepositoryImplTest.kt
+++ data/src/test/java/com/davidcastella/data/conversionrates/repositories/ConversionRatesRepositoryImplTest.kt
@@ -9,12 +9,11 @@ import com.davidcastella.domain.core.failure.Failure
 import io.mockk.coEvery
 import io.mockk.coVerify
 import io.mockk.mockk
-import kotlinx.coroutines.flow.collect
+import java.net.UnknownHostException
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Test
-import java.net.UnknownHostException
 
 class ConversionRatesRepositoryImplTest {
 
@@ -34,56 +33,60 @@ class ConversionRatesRepositoryImplTest {
     }
 
     @Test
-    fun `given repository when call getConversionRates method succeeds then return correct data`() = runBlocking {
-        coEvery { datasource.getConversionRates() } returns listOf(conversionRateModel)
+    fun `given repository when call getConversionRates method succeeds then return correct data`() =
+        runBlocking {
+            coEvery { datasource.getConversionRates() } returns listOf(conversionRateModel)
 
-        val result = repository.getConversionRates()
+            val result = repository.getConversionRates()
 
-        result.collect {
-            Assert.assertEquals(conversionRate, (it as Either.Right).value.first())
+            result.collect {
+                Assert.assertEquals(conversionRate, (it as Either.Right).value.first())
+            }
+
+            coVerify(exactly = 1) { datasource.getConversionRates() }
+            coVerify(exactly = 1) { mapper.invoke(any()) }
         }
-
-        coVerify(exactly = 1) { datasource.getConversionRates() }
-        coVerify(exactly = 1) { mapper.invoke(any()) }
-    }
 
     @Test
-    fun `given repository when call getConversionRates method fails then return correct generic failure`() = runBlocking {
-        coEvery { datasource.getConversionRates() } throws Exception()
-        val result = repository.getConversionRates()
+    fun `given repository when call getConversionRates method fails then return correct generic failure`() =
+        runBlocking {
+            coEvery { datasource.getConversionRates() } throws Exception()
+            val result = repository.getConversionRates()
 
-        result.collect {
-            Assert.assertEquals(Failure.GENERIC_FAILURE, (it as Either.Left).value)
+            result.collect {
+                Assert.assertEquals(Failure.GENERIC_FAILURE, (it as Either.Left).value)
+            }
+
+            coVerify(exactly = 1) { datasource.getConversionRates() }
+            coVerify(exactly = 0) { mapper.invoke(any()) }
         }
-
-        coVerify(exactly = 1) { datasource.getConversionRates() }
-        coVerify(exactly = 0) { mapper.invoke(any()) }
-    }
 
     @Test
-    fun `given repository when call getConversionRates method fails then return correct connection failure`() = runBlocking {
-        coEvery { datasource.getConversionRates() } throws UnknownHostException()
-        val result = repository.getConversionRates()
+    fun `given repository when call getConversionRates method fails then return correct connection failure`() =
+        runBlocking {
+            coEvery { datasource.getConversionRates() } throws UnknownHostException()
+            val result = repository.getConversionRates()
 
-        result.collect {
-            Assert.assertEquals(Failure.CONNECTION_FAILURE, (it as Either.Left).value)
+            result.collect {
+                Assert.assertEquals(Failure.CONNECTION_FAILURE, (it as Either.Left).value)
+            }
+
+            coVerify(exactly = 1) { datasource.getConversionRates() }
+            coVerify(exactly = 0) { mapper.invoke(any()) }
         }
-
-        coVerify(exactly = 1) { datasource.getConversionRates() }
-        coVerify(exactly = 0) { mapper.invoke(any()) }
-    }
 
     @Test
-    fun `given repository when call getConversionRates method fails then return correct http failure`() = runBlocking {
-        coEvery { datasource.getConversionRates() } throws RuntimeException()
+    fun `given repository when call getConversionRates method fails then return correct http failure`() =
+        runBlocking {
+            coEvery { datasource.getConversionRates() } throws RuntimeException()
 
-        val result = repository.getConversionRates()
+            val result = repository.getConversionRates()
 
-        result.collect {
-            Assert.assertEquals(Failure.HTTP_FAILURE, (it as Either.Left).value)
+            result.collect {
+                Assert.assertEquals(Failure.HTTP_FAILURE, (it as Either.Left).value)
+            }
+
+            coVerify(exactly = 1) { datasource.getConversionRates() }
+            coVerify(exactly = 0) { mapper.invoke(any()) }
         }
-
-        coVerify(exactly = 1) { datasource.getConversionRates() }
-        coVerify(exactly = 0) { mapper.invoke(any()) }
-    }
 }

85af221d0b0f63270f51c06213833553dbad10eb
David Castellà <hola@davidcastella.com>
2023-11-01 22:22:45 +0100

    Improve algorithm

--- domain/src/test/java/com/davidcastella/domain/conversionrates/util/GetConversionRateKtTest.kt
+++ domain/src/test/java/com/davidcastella/domain/conversionrates/util/GetConversionRateKtTest.kt
@@ -13,30 +13,45 @@ class GetConversionRateKtTest {
     @Before
     fun setUp() {
         list = listOf(
-            ConversionRate("AUD", "EUR", 1.27.toBigDecimal()),
-            ConversionRate("EUR", "AUD", 0.79.toBigDecimal()),
-            ConversionRate("AUD", "CAD", 1.06.toBigDecimal()),
-            ConversionRate("CAD", "AUD", 0.94.toBigDecimal()),
-            ConversionRate("EUR", "USD", 0.64.toBigDecimal()),
-            ConversionRate("USD", "EUR", 1.56.toBigDecimal())
+            ConversionRate(from="EUR", to="USD", rate=1.04.toBigDecimal()),
+            ConversionRate(from="USD", to="EUR", rate=0.96.toBigDecimal()),
+            ConversionRate(from="GBP", to="EUR", rate=1.16.toBigDecimal()),
+            ConversionRate(from="JPY", to="USD", rate=0.0073.toBigDecimal()),
+            ConversionRate(from="AUD", to="INR", rate=55.29.toBigDecimal()),
+            ConversionRate(from="CAD", to="USD", rate=0.74.toBigDecimal()),
+            ConversionRate(from="SEK", to="USD", rate=0.096.toBigDecimal()),
+            ConversionRate(from="RUB", to="SEK", rate=0.17.toBigDecimal()),
+            ConversionRate(from="INR", to="EUR", rate=0.012.toBigDecimal())
         )
     }
 
     @Test
     fun getDirectConversionRate() {
-        val rate = list.getConversionRate("EUR", "USD")
-        val rate2 = list.getConversionRate("CAD", "AUD")
+        val rate = list.getConversionRate("USD", "EUR")
+        val rate2 = list.getConversionRate("CAD", "USD")
 
-        assertEquals(0.64.toBigDecimal(), rate)
-        assertEquals(0.94.toBigDecimal(), rate2)
+        assertEquals(0.96.toBigDecimal(), rate)
+        assertEquals(0.74.toBigDecimal(), rate2)
     }
 
     @Test
-    fun getIndirectConversionRate() {
+    fun getIndirectConversionRate1() {
         val rate = list.getConversionRate("CAD", "EUR")
-        val rate2 = list.getConversionRate("USD", "CAD")
 
-        assertEquals(1.1938.toBigDecimal(), rate)
-        assertEquals(1.306344.toBigDecimal(), rate2)
+        assertEquals(0.7104.toBigDecimal(), rate)
+    }
+
+    @Test
+    fun getIndirectConversionRate2() {
+        val rate2 = list.getConversionRate("RUB", "EUR")
+
+        assertEquals(0.0156672.toBigDecimal(), rate2)
+    }
+
+    @Test
+    fun getIndirectConversionRate3() {
+        val rate2 = list.getConversionRate("USD", "USD")
+
+        assertEquals(1.toBigDecimal(), rate2)
     }
 }

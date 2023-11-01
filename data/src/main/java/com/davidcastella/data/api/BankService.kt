422b3e1b6390c2386e916c678154ceb8107e66fe
David Castellà <hola@davidcastella.com>
2023-11-01 22:23:59 +0100

    Remove extra space

--- data/src/main/java/com/davidcastella/data/api/BankService.kt
+++ data/src/main/java/com/davidcastella/data/api/BankService.kt
@@ -3,7 +3,6 @@ package com.davidcastella.data.api
 import com.davidcastella.data.api.models.ConversionRateResponseModel
 import com.davidcastella.data.api.models.TransactionResponseModel
 
-
 interface BankService {
     suspend fun getTransactions(): List<TransactionResponseModel>
     suspend fun getCurrencyRates(): List<ConversionRateResponseModel>

2dc7cb132c4e6eb078e6f4debd62a9daac30f321
David Castellà <hola@davidcastella.com>
2023-11-01 23:11:49 +0100

    Clean code

--- domain/src/main/java/com/davidcastella/domain/conversionrates/util/getConversionRate.kt
+++ domain/src/main/java/com/davidcastella/domain/conversionrates/util/getConversionRate.kt
@@ -3,12 +3,12 @@ package com.davidcastella.domain.conversionrates.util
 import com.davidcastella.domain.conversionrates.entities.ConversionRate
 import java.math.BigDecimal
 
-fun List<ConversionRate>.getConversionRate(from: String, to: String, list: List<ConversionRate> = this): BigDecimal {
-    val rest = list.firstOrNull { it.from == from && it.to == to }
+fun List<ConversionRate>.getConversionRate(from: String, to: String): BigDecimal {
+    val rest = firstOrNull { it.from == from && it.to == to }
     return rest?.rate
         ?: if (from == to) BigDecimal(1)
         else {
-            val n = list.first { it.from == from }
-            n.rate * list.filter { it != n && it.to != from }.getConversionRate(n.to, to)
+            val n = first { it.from == from }
+            n.rate * filter { it != n && it.to != from }.getConversionRate(n.to, to)
         }
 }
